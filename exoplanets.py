942c39e3f3dca534059c33364e6f9a54b77e4666
Ronit Godara <choudharyronit984@gmail.com>
2026-06-26 08:56:57 +0530

    Refactor exoplanets.py by re-adding imports and config

--- exoplanets.py
+++ exoplanets.py
@@ -78,7 +78,7 @@ st.write("Explore planets beyond our Solar System.")
 conn = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="1234",      # Put your MySQL password here
+    password="Your_Pass",      # Put your MySQL password here
     database="space"
 )
 query = """
